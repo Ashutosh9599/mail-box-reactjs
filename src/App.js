c14ca49eaa5dd058f0480065304a980b053db4c8
Ashutosh9599 <ashutoshdas091@gmail.com>
2024-02-20 20:38:27 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import SignUpPage from './Auth/Signup';
+import LoginPage from './Auth/Login';
+import Welcome from './Pages/Welcome';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
-          <Routes>
-            <Route path="/" element={<SignUpPage />} />
-          </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
     </Router>
   );
 }
